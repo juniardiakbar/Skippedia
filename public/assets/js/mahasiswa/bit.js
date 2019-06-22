e875a19b0002cdbe614ec1b2ce78d5f27f5e3410
Frankuy <fhizbullah99@gmail.com>
2019-06-22 16:02:04 +0700

    Debug pagination while sorting data

--- public/assets/js/mahasiswa/bit.js
+++ public/assets/js/mahasiswa/bit.js
@@ -12,78 +12,120 @@ function ajax(_url){
 
 this.csrf = csrf.value;
 axios.defaults.headers.common['X-CSRF-TOKEN'] = this.csrf;
-var page = $('#page').val();
-var limit = $('#limit').val();
-var search = $('#search').val();
-var sort = $('#sort').val();
-var method = $('#method').val();
 
 var url = '/mahasiswa/all?'
-var query = 'page='+page+'&limit='+limit+'&search='+search+'&angkatan=bit';
 
 $(document).on('click','#namaASC', function(){
+  var page = $('#page').val();
+  var limit = $('#limit').val();
+  var search = $('#search').val();
+
+  var query = 'page='+page+'&limit='+limit+'&search='+search+'&angkatan=bit';
+
 	ajax(url+'sort=nama&method=DESC&'+query+'&jurusan=all');
 	$('#namaASC').attr('id', 'namaDESC');
   $('#icon-sort').remove();
-  $('#namaDESC').append('<i class="fa fa-arrow-down" id="icon-sort"></i>');
+  $('#namaDESC').append('<i class="fas fa-sort-alpha-up fa-lg fa-lg" id="icon-sort"></i>');
   $('#sort').val('nama');
   $('#method').val('DESC');
 });
 $(document).on('click','#namaDESC', function(){
+  var page = $('#page').val();
+  var limit = $('#limit').val();
+  var search = $('#search').val();
+
+  var query = 'page='+page+'&limit='+limit+'&search='+search+'&angkatan=bit';
+
 	ajax(url+'sort=nama&method=ASC&'+query+'&jurusan=all');
 	$('#namaDESC').attr('id', 'namaASC');
   $('#icon-sort').remove();
-  $('#namaASC').append('<i class="fa fa-arrow-up" id="icon-sort"></i>');
+  $('#namaASC').append('<i class="fas fa-sort-alpha-down fa-lg" id="icon-sort"></i>');
   $('#sort').val('nama');
   $('#method').val('ASC');
 });
 $(document).on('click','#nama', function(){
+  var page = $('#page').val();
+  var limit = $('#limit').val();
+  var search = $('#search').val();
+
+  var query = 'page='+page+'&limit='+limit+'&search='+search+'&angkatan=bit';
+
 	ajax(url+'sort=nama&method=ASC&'+query+'&jurusan=all');
 	$('#nama').attr('id', 'namaASC');
   $('#icon-sort').remove();
-  $('#namaASC').append('<i class="fa fa-arrow-up" id="icon-sort"></i>')
+  $('#namaASC').append('<i class="fas fa-sort-alpha-down fa-lg" id="icon-sort"></i>')
   $('#sort').val('nama');
   $('#method').val('ASC');
 });
 
 $(document).on('click','#nimASC', function(){
+  var page = $('#page').val();
+  var limit = $('#limit').val();
+  var search = $('#search').val();
+
+  var query = 'page='+page+'&limit='+limit+'&search='+search+'&angkatan=bit';
+
 	ajax(url+'sort=nim&method=DESC&'+query+'&jurusan=all');
 	$('#nimASC').attr('id', 'nimDESC');
   $('#icon-sort').remove();
-  $('#nimDESC').append('<i class="fa fa-arrow-down" id="icon-sort"></i>');
+  $('#nimDESC').append('<i class="fas fa-sort-numeric-up fa-lg" id="icon-sort"></i>');
   $('#sort').val('nim');
   $('#method').val('DESC');
 });
 $(document).on('click','#nimDESC', function(){
+  var page = $('#page').val();
+  var limit = $('#limit').val();
+  var search = $('#search').val();
+
+  var query = 'page='+page+'&limit='+limit+'&search='+search+'&angkatan=bit';
+
 	ajax(url+'sort=nim&method=ASC&'+query+'&jurusan=all');
 	$('#nimDESC').attr('id', 'nimASC');
   $('#icon-sort').remove();
-  $('#nimASC').append('<i class="fa fa-arrow-up" id="icon-sort"></i>')
+  $('#nimASC').append('<i class="fas fa-sort-numeric-down fa-lg" id="icon-sort"></i>')
   $('#sort').val('nim');
   $('#method').val('ASC');
 });
 
 $(document).on('click','#ratingASC', function(){
+  var page = $('#page').val();
+  var limit = $('#limit').val();
+  var search = $('#search').val();
+
+  var query = 'page='+page+'&limit='+limit+'&search='+search+'&angkatan=bit';
+
   ajax(url+'sort=rating&method=DESC&'+query+'&jurusan=all');
   $('#ratingASC').attr('id', 'ratingDESC');
   $('#icon-sort').remove();
-  $('#ratingDESC').append('<i class="fa fa-arrow-down" id="icon-sort"></i>')
+  $('#ratingDESC').append('<i class="fas fa-sort-numeric-up fa-lg" id="icon-sort"></i>')
   $('#sort').val('rating');
   $('#method').val('DESC');
 });
 $(document).on('click','#ratingDESC', function(){
+  var page = $('#page').val();
+  var limit = $('#limit').val();
+  var search = $('#search').val();
+
+  var query = 'page='+page+'&limit='+limit+'&search='+search+'&angkatan=bit';
+
   ajax(url+'sort=rating&method=ASC&'+query+'&jurusan=all');
   $('#ratingDESC').attr('id', 'ratingASC');
   $('#icon-sort').remove();
-  $('#ratingASC').append('<i class="fa fa-arrow-up" id="icon-sort"></i>')
+  $('#ratingASC').append('<i class="fas fa-sort-numeric-down fa-lg" id="icon-sort"></i>')
   $('#sort').val('rating');
   $('#method').val('ASC');
 });
 $(document).on('click','#rating', function(){
+  var page = $('#page').val();
+  var limit = $('#limit').val();
+  var search = $('#search').val();
+
+  var query = 'page='+page+'&limit='+limit+'&search='+search+'&angkatan=bit';
+
   ajax(url+'sort=rating&method=ASC&'+query+'&jurusan=all');
   $('#rating').attr('id', 'ratingASC');
   $('#icon-sort').remove();
-  $('#ratingASC').append('<i class="fa fa-arrow-up" id="icon-sort"></i>')
+  $('#ratingASC').append('<i class="fas fa-sort-numeric-down fa-lg" id="icon-sort"></i>')
   $('#sort').val('rating');
   $('#method').val('ASC');
 });
@@ -92,6 +134,10 @@ $('#prev').on('click', function() {
   if ($('#page').val() != '1'){
     $('#page').val(parseInt($('#page').val()) - 1);
     var page = $('#page').val();
+    var limit = $('#limit').val();
+    var search = $('#search').val();
+    var sort = $('#sort').val();
+    var method = $('#method').val();
 
     var query = 'page='+page+'&limit='+limit+'&search='+search+'&angkatan=bit';
     ajax(url+'sort='+sort+'&method='+method+'&'+query+'&jurusan=all');
@@ -107,6 +153,10 @@ $('#next').on('click', function() {
   if ($('#page').val() != $('#pages').val() && $('#pages').val() != '1'){
     $('#page').val(parseInt($('#page').val()) + 1);
     var page = $('#page').val();
+    var limit = $('#limit').val();
+    var search = $('#search').val();
+    var sort = $('#sort').val();
+    var method = $('#method').val();
 
     var query = 'page='+page+'&limit='+limit+'&search='+search+'&angkatan=bit';
     ajax(url+'sort='+sort+'&method='+method+'&'+query+'&jurusan=all');
@@ -119,7 +169,11 @@ $('#next').on('click', function() {
 });
 
 $('#search').keyup(function() {
+  var limit = $('#limit').val();
+  var sort = $('#sort').val();
+  var method = $('#method').val();
   var search = $('#search').val();
+  
   $('#page').val(1);
   var page = 1;
   $('#number').text(page);  
